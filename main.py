5b054a499a7c7149daf3bead3a6afcf40b4d476c
amRadhwen <radhweneamri9631@gmail.com>
2021-08-28 16:44:42 +0100

    pieces created

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from numpy import zeros, flipud
 import pygame
+import math
 
 WIDTH = 600
 HEIGHT = 600
@@ -7,10 +8,16 @@ SIZE = (WIDTH, HEIGHT)
 DIMENSIONS = (10, 10)
 SURFACE = DIMENSIONS[0]*DIMENSIONS[1]
 PIECESNUMBER = int((DIMENSIONS[0]-2)/2)
-
+SQUAREWIDTH = int(WIDTH / DIMENSIONS[0])
+SQUAREHEIGHT = int(HEIGHT / DIMENSIONS[1])
+BLACKPIECE = pygame.image.load("b_piece.png")
+WHITEPIECE = pygame.image.load("w_piece.png")
 #colors
+#         R   G   B
 LIGHT = (254,206,158)
-DARK = (209,139,70)
+DARK  = (209,139, 70)
+BLACK = (  0,  0,  0)
+WHITE = (255,255,255)
 
 def createBoard(dimensions):
     return flipud(zeros(dimensions))
@@ -22,9 +29,9 @@ def createBoardCoords():
     board_coords = []
     y = 0
     while y < HEIGHT:
-        for x in range(0, HEIGHT, 75):
+        for x in range(0, HEIGHT, SQUAREWIDTH):
             board_coords.append([x, y])
-        y += 75
+        y += SQUAREWIDTH
     return board_coords
 
 def initBoard(board):
@@ -85,34 +92,35 @@ def eat(board, player, origin, moves, choice):
             if moves.__len__() == 1:
                 if choice == moves[0]:
                     board[origin[0]][origin[1]]  = 0
-                    board[moves[0][0]][moves[0][1]] = 2 #same as using choice in place of moves
+                    board[moves[0][0]][moves[0][1]] = 2 #same as using choice in place of moves with single index
                     return True
                 else:
                     return False
             else:
                 if choice in moves:
                     board[origin[0]][origin[1]]  = 0
-                    board[choice[0][0]][choice[0][1]] = 2
+                    board[choice[0]][choice[1]] = 2
                     return True
         else:
             return False
     else:
         if player == 1:
-        if moves:
-            if moves.__len__() == 1:
-                if choice == moves[0]:
-                    board[origin[0]][origin[1]]  = 0
-                    board[moves[0][0]][moves[0][1]] = 1 #same as using choice in place of moves
-                    return True
+            if moves:
+                if moves.__len__() == 1:
+                    if choice == moves[0]:
+                        board[origin[0]][origin[1]]  = 0
+                        board[moves[0][0]][moves[0][1]] = 1 #same as using choice in place of moves with single index
+                        return True
+                    else:
+                        return False
                 else:
-                    return False
+                    if choice in moves:
+                        board[origin[0]][origin[1]]  = 0
+                        board[choice[0]][choice[1]] = 1
+                        return True
             else:
-                if choice in moves:
-                    board[origin[0]][origin[1]]  = 0
-                    board[choice[0][0]][choice[0][1]] = 1
-                    return True
-        else:
-            return False
+                return False
+
     
 
 
@@ -120,15 +128,20 @@ def eat(board, player, origin, moves, choice):
 def drawBoard():
     coords = createBoardCoords()
     color = (DARK, LIGHT)
+    disk_color = (BLACKPIECE, WHITEPIECE)
     color_inv = False
     row_inv = 1
     c = 0
 
     while c < coords.__len__():
-        if row_inv > 8:
+        if row_inv > DIMENSIONS[0]:
             color_inv = not color_inv
             row_inv = 1
-        pygame.draw.rect(screen, color[int(color_inv)], [coords[c][0], coords[c][1], 75, 75])
+        pygame.draw.rect(screen, color[int(color_inv)], [coords[c][0], coords[c][1], SQUAREWIDTH, SQUAREHEIGHT])
+        if c < 40 and color_inv == 0:
+            screen.blit(disk_color[color_inv], [coords[c][0]+5, coords[c][1]+2])
+        elif c > 60 and color_inv == 1:
+            screen.blit(disk_color[color_inv], [coords[c][0]+5, coords[c][1]+2])
         color_inv = not color_inv
         c += 1
         row_inv += 1
@@ -137,34 +150,71 @@ board = createBoard(DIMENSIONS)
 
 initBoard(board)
 print(board)
-print(checkMove(board, [6, 1]).__len__())
-print(checkMove(board, [0, 9]).__len__())
 
-
-#pygame.init()
-#screen = pygame.display.set_mode(SIZE)
-#pygame.display.set_caption("Damme V1.0")
-#
-##loop controller
 #gameEnd = False
 #
-##clock
-#clock = pygame.time.Clock()
-#
-##change the screen color to LIGHT
-#screen.fill(LIGHT)
-#drawBoard()
-#print(createBoard(DIMENSIONS))
-#
-#
-##update the display
-#pygame.display.update()
-#
-##Game main loop
 #while not gameEnd:
-#    for e in pygame.event.get():
-#        if e.type == pygame.QUIT:
-#            gameEnd = True
+#    p1 = list(input("Player one choose (exp: 1,0): "))
+#    p1 = [int(p1[0]), int(p1[2])]
+#    possibleMoves = checkMove(board, p1)
+#    if possibleMoves:
+#        choice = list(input("Move To ("+str(possibleMoves)+")"))
+#        choice = [int(choice[0]), int(choice[2])]
+#        print(choice)
+#        eat(board, 1, p1, possibleMoves, choice)
 #
-#
-#pygame.quit()
+#    print(board)
+#    
+#    p2 = list(input("Player two choose (exp: 1,0): "))
+#    p2 = [int(p2[0]), int(p2[2])]
+#    possibleMoves = checkMove(board, p2)
+#    if possibleMoves:
+#        choice = list(input("Move To ("+str(possibleMoves)+")"))
+#        choice = [int(choice[0]), int(choice[2])]
+#        eat(board, 2, p2, possibleMoves, choice)
+#    
+#    print(board)
+
+
+            
+        
+
+
+
+pygame.init()
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("Damme V1.0")
+pygame.display.set_icon(pygame.image.load("dames.ico"))
+
+#loop controller
+gameEnd = False
+
+#clock
+clock = pygame.time.Clock()
+
+#change the screen color to LIGHT
+screen.fill(LIGHT)
+drawBoard()
+print(createBoard(DIMENSIONS))
+
+
+#update the display
+pygame.display.update()
+
+#Game main loop
+while not gameEnd:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            gameEnd = True
+            
+        if e.type == pygame.MOUSEBUTTONDOWN:
+            posx = e.pos[0]
+            posy = e.pos[1]
+            x = int(math.floor(posx/SQUAREWIDTH))
+            y = int(math.floor(posy/SQUAREHEIGHT))
+            print([x, y])
+            
+
+
+
+pygame.quit()
